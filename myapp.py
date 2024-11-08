5f2f83558ab4a55ff5593c10aaea30ee959e6b3e
AdityaaMittal <aditya.mittal.2554@gmail.com>
2024-11-08 22:49:10 +0530

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -1,9 +1,15 @@
-from flask import Flask
-app = Flask("My App")
+from flask import Flask, render_template, jsonify
 
-@app.route("/", methods=["GET"])
-def welcome():
-    return "<h1>Welcome to my app!</h1>"
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('myapp.html')
+
+@app.route('/button_action')
+def button_action():
+    # Return a JSON response that will be displayed in the HTML
+    return jsonify(message="Button was clicked!")
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
